cca92e0940118b9f2db2b27a9b970c87696c98ff
Adam Spotton <aspotton@gmail.com>
2017-01-22 15:34:54 -0500

    Improved Python Screenshot example: added parameters

--- screenshots/python/basic_authentication_using_requests.py
+++ screenshots/python/basic_authentication_using_requests.py
@@ -9,16 +9,19 @@ secret_key = "<insert your API secret key>"
 
 # For additional image parameters and possible image sizes
 # see: http://docs.webshrinker.com/v2/website-screenshot-api.html#image-requests
-image_size = "3xlarge"
+parameters = {
+    'size': '3xlarge',
+    'fullpage': True
+}
 
-api_url = "https://api.webshrinker.com/thumbnails/v2/%s?size=%s" % (urlsafe_b64encode(target_website), image_size)
+api_url = "https://api.webshrinker.com/thumbnails/v2/%s" % urlsafe_b64encode(target_website)
 
-response = requests.get(api_url, auth=(key, secret_key), stream=True)
+response = requests.get(api_url, auth=(key, secret_key), stream=True, params=parameters)
 status_code = response.status_code
 
 if status_code == 200:
     # Do something with the image response
-    print "Saving the screenshot for '%s' to 'screenshot_demo.png'"
+    print "Saving the screenshot for '%s' to 'screenshot_demo.png'" % target_website
     with open('screenshot_demo.png', 'wb') as image_file:
         shutil.copyfileobj(response.raw, image_file)
 elif status_code == 202:

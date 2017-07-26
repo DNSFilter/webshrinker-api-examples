4c00197d1ebd86246c63f65593cc3dec3a3d1a90
Adam Spotton <aspotton@gmail.com>
2017-07-26 14:43:41 -0400

    Updated Python category examples for API v3

--- categories/python/basic_authentication_using_requests.py
+++ categories/python/basic_authentication_using_requests.py
@@ -6,7 +6,7 @@ target_website = "<the URL or domain name of the website to retrieve the categor
 key = "<insert your API key>"
 secret_key = "<insert your API secret key>"
 
-api_url = "https://api.webshrinker.com/categories/v2/%s" % urlsafe_b64encode(target_website)
+api_url = "https://api.webshrinker.com/categories/v3/%s" % urlsafe_b64encode(target_website)
 
 response = requests.get(api_url, auth=(key, secret_key))
 status_code = response.status_code
@@ -14,8 +14,14 @@ data = response.json()
 
 if status_code == 200:
     # Do something with the JSON response
-    categories = data['data'][0]['categories']
-    print "'%s' belongs to the following categories: %s" % (target_website, ",".join(categories))
+    category_data = data['data'][0]['categories']
+
+    # Build a string array containing the category ID, the human friendly category name, score and confident values for each entry
+    categories = []
+    for entry in category_data:
+        categories.append('({}) {} [score={},confident={}]'.format(entry['id'], entry['label'], entry['score'], entry['confident']))
+
+    print "'%s' belongs to the following categories:\n%s" % (target_website, "\n".join(categories))
 elif status_code == 202:
     # The request is being categorized right now in real time, check again for an updated answer
     print "The categories for '%s' are being determined, check again in a few seconds" % target_website

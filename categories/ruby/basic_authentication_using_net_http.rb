45589d503ed231654e4ad561b46d161746605e31
Adam Spotton <aspotton@gmail.com>
2017-07-26 15:09:15 -0400

    Updated category Ruby examples for API v3

--- categories/ruby/basic_authentication_using_net_http.rb
+++ categories/ruby/basic_authentication_using_net_http.rb
@@ -7,7 +7,7 @@ target_website = '<the URL or domain name of the website to retrieve the categor
 key = '<insert your API key>'
 secret_key = '<insert your API secret key>'
 
-api_uri = URI('https://api.webshrinker.com/categories/v2/' + Base64.urlsafe_encode64(target_website))
+api_uri = URI('https://api.webshrinker.com/categories/v3/' + Base64.urlsafe_encode64(target_website))
 
 req = Net::HTTP::Get.new(api_uri)
 req.basic_auth key, secret_key
@@ -21,13 +21,20 @@ data = JSON.parse(res.body)
 
 if status_code == '200'
     # Do something with the JSON response
-    categories = data['data'][0]['categories']
-    puts "'#{target_website}' belongs to the following categories: #{categories}"
+    category_data = data['data'][0]['categories']
+
+    puts "'#{target_website}' belongs to the following categories:"
+
+    # Print the category ID, the human friendly category name, score and confident values for each entry
+    categories = []
+    category_data.each {|entry|
+        puts "(%s) %s [score=%f,confident=%s]" % [entry['id'], entry['label'], entry['score'], entry['confident']]
+    }
 elsif status_code == '202'
     # The request is being categorized right now in real time, check again for an updated answer
     puts "The categories for '#{target_website}' are being determined, check again in a few seconds"
 else
-    # The different status codes are covered in the documentation (http://docs.webshrinker.com/v2/website-category-api.html#category-lookup
+    # The different status codes are covered in the documentation (https://docs.webshrinker.com/v3/website-category-api.html)
     puts "An error occurred: HTTP #{status_code}"
     if data.has_key? 'error'
     	puts data['error']['message']
